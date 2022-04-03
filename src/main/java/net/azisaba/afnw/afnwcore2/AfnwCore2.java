cd386ef199e2bc12f60e52ce705b87f3fec9f446
merunno <merunno92@gmail.com>
2022-04-03 15:35:44 +0900

    first commit

---
+++ src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
@@ -0,0 +1,17 @@
+package net.azisaba.afnw.afnwcore2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AfnwCore2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
