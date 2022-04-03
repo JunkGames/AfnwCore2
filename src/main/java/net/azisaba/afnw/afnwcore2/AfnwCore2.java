9f531337963c7b697d2b27d22aae8b76fdb97676
merunno <merunno92@gmail.com>
2022-04-04 02:29:33 +0900

    feat: add register events

--- src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
@@ -3,6 +3,8 @@ package net.azisaba.afnw.afnwcore2;
 import net.azisaba.afnw.afnwcore2.listeners.player.FirstPlayerJoinListener;
 import net.azisaba.afnw.afnwcore2.listeners.player.JoinListener;
 import net.azisaba.afnw.afnwcore2.listeners.player.QuitListener;
+import net.azisaba.afnw.afnwcore2.listeners.player.block.CropsBreakCanceller;
+import net.azisaba.afnw.afnwcore2.listeners.player.block.SaplingBreakCanceller;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,9 +15,13 @@ public class AfnwCore2 extends JavaPlugin {
     public void onEnable() {
         // register listeners
         PluginManager pluginEvent = Bukkit.getPluginManager();
+        /* listeners - player */
         pluginEvent.registerEvents(new JoinListener(), this);
         pluginEvent.registerEvents(new QuitListener(), this);
         pluginEvent.registerEvents(new FirstPlayerJoinListener(), this);
+        /* listeners - block */
+        pluginEvent.registerEvents(new CropsBreakCanceller(), this);
+        pluginEvent.registerEvents(new SaplingBreakCanceller(), this);
 
         getLogger().info("[AfnwCore2] Enabled!");
     }
