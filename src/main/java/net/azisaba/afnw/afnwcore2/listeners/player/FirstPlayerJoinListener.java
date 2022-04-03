623132ae731aa17d12d91cd5db0c747e2d8ae524
merunno <merunno92@gmail.com>
2022-04-03 23:57:48 +0900

    feat: add PlayerJoinEvent(if first player)

---
+++ src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/FirstPlayerJoinListener.java
@@ -0,0 +1,29 @@
+package net.azisaba.afnw.afnwcore2.listeners.player;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class FirstPlayerJoinListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        if(p.hasPlayedBefore()) return;
+
+        String[] firstMessage = {
+                ChatColor.GOLD +
+                """
+                Afnwへようこそ!
+                奈落の世界を開拓するマルチサーバーです。まず初めにチュートリアルワールドを進んでみましょう。
+                なお、アジ鯖公式Wikiをご覧ください。
+                https://wiki.azisaba.net/wiki/
+                """
+        };
+
+        p.sendMessage(firstMessage);
+    }
+}
