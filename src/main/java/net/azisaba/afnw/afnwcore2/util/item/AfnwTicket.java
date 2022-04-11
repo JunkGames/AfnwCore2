46c43a8ea27af86d1e2d132ec46019e4972dbf37
merunno <merunno92@gmail.com>
2022-04-11 20:41:03 +0900

    feat: add AfnwTicket

---
+++ src/main/java/net/azisaba/afnw/afnwcore2/util/item/AfnwTicket.java
@@ -0,0 +1,29 @@
+package net.azisaba.afnw.afnwcore2.util.item;
+
+import net.azisaba.afnw.afnwcore2.AfnwCore2;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AfnwTicketを作成する
+ * @param plugin
+ */
+public record AfnwTicket(AfnwCore2 plugin) {
+
+    public static ItemStack afnwTicket = new ItemStack(Material.PAPER, 1);
+    static {
+        ItemMeta metaData = afnwTicket.getItemMeta();
+        metaData.displayName(Component.text("AfnwTicket").color(NamedTextColor.LIGHT_PURPLE));
+        List<String> lore = new ArrayList<>();
+        lore.add("Afnw内で使用できるチケットです。");
+        lore.add("/afnw でチケットとアイテムを交換できます。また、このアイテムは交換可能です。");
+        metaData.setLore(lore);
+        afnwTicket.setItemMeta(metaData);
+    }
+}
