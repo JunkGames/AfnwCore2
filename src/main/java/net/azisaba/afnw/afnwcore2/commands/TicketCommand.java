b62f9726ae54f48b8c795a12a0b2a4811d03ba41
merunno <merunno92@gmail.com>
2022-05-19 18:40:29 +0900

    chore: チケット送信コマンドの追加

--- src/main/java/net/azisaba/afnw/afnwcore2/commands/TicketCommand.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/commands/TicketCommand.java
@@ -52,8 +52,37 @@ public record TicketCommand(JavaPlugin plugin) implements CommandExecutor {
                 // 成功した趣旨の情報送信
                 sender.sendMessage(Component.text(sendTarget.getName() + "へのチケット送信に成功しました。配布数:" + ticketSize));
                 // 通知
-                sendTarget.sendMessage(Component.text("* チケットを入手しました。/afnwを実行することでアイテムと交換することができます。", NamedTextColor.YELLOW));
+                sendTarget.sendMessage(Component.text("チケットを入手しました。/afnwを実行することでアイテムと交換することができます。", NamedTextColor.YELLOW));
             }
+            case "give" -> {
+                if(!(args.length == 3)) {
+                    sender.sendMessage(Component.text("Usage(give): /ticket give <size> <player>", NamedTextColor.RED));
+                    return true;
+                }
+
+                int giveTicketInt = Integer.parseInt(args[1]);
+                Player giveTarget = Bukkit.getPlayerExact(args[2]);
+                if(giveTarget == null) {
+                    sender.sendMessage(Component.text("対象のプレイヤーを発見することができませんでした", NamedTextColor.RED));
+                    break;
+                }
+                if(giveTicketInt <= 0) {
+                    sender.sendMessage(Component.text("配布数は0以上を指定する必要があります。", NamedTextColor.RED));
+                    break;
+                }
+
+                for(int i = 0; i < giveTicketInt; i++) {
+                    giveTarget.getInventory().addItem(AfnwTicket.afnwTicket);
+                }
+
+                // 成功した趣旨の情報送信
+                Bukkit.getLogger().info(sender.getName() + "が" + giveTarget.getName() + "にチケットを配布しました。: " + giveTicketInt + "枚");
+                sender.sendMessage(Component.text(giveTarget.getName() + "に" + giveTicketInt + "枚のチケットを配布しました。", NamedTextColor.YELLOW));
+                // 通知
+                giveTarget.sendMessage(Component.text("チケットを入手しました。(補填)/afnwを実行することでアイテムと交換することができます。", NamedTextColor.YELLOW));
+            }
+
+            // TODO: 連続投票ボーナス数の表示処理の作成 : /ticket show
         }
         return true;
     }
