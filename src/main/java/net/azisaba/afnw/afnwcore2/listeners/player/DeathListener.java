f39695357430edec25c1a0d6fb3a9cbd30b52a71
Meru <merunno92@gmail.com>
2022-05-19 17:38:39 +0900

    feat: 死亡メッセージの追加 (#14)
    * chore: use paper api - v1.17.1
    
    * feat: add dropped exp info
    
    * chore: bump version

---
+++ src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/DeathListener.java
@@ -0,0 +1,25 @@
+package net.azisaba.afnw.afnwcore2.listeners.player;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class DeathListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onDeath(PlayerDeathEvent e) {
+        Player p = e.getPlayer();
+
+        // ドロップした経験値量を通知
+        int dropExp = e.getDroppedExp();
+        if(dropExp == 0) {
+            p.sendMessage(Component.text("死亡しました。経験値の消費はありません。", NamedTextColor.RED));
+            return;
+        }
+        p.sendMessage(Component.text("死亡したため、" + e.getDroppedExp() + " Expを失いました。", NamedTextColor.RED));
+    }
+}
