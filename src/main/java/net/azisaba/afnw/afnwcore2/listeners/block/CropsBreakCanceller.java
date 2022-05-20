f13688e2ead1c1fb3318e45b30a7ff9d02e357ff
merunno <merunno92@gmail.com>
2022-05-21 00:44:21 +0900

    feat: パーミッションの置き換え

--- src/main/java/net/azisaba/afnw/afnwcore2/listeners/block/CropsBreakCanceller.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/listeners/block/CropsBreakCanceller.java
@@ -31,7 +31,7 @@ public class CropsBreakCanceller implements Listener {
         Block b = e.getBlock();
 
         if(!Tag.CROPS.isTagged(b.getLocation().add(0, 1, 0).getBlock().getType())) return;
-        if(p.hasPermission("afnw.bypass.break.crops")) {
+        if(p.hasPermission("afnw.bypass.break.arable")) {
             p.sendMessage(Component.text("農作物保護機能(crops)をbypassしました。").color(NamedTextColor.GOLD));
             Logger.getLogger("bypass:crops").info(p.getName() + "break crops! (bypass canceller!)");
         }
