b78fa0b020c11614c52a3ed300e4189c36a89e47
m2en <merunno92@gmail.com>
2022-06-30 17:31:21 +0900

    docs: javadocの追加

--- src/main/java/net/azisaba/afnw/afnwcore2/util/data/PlayerData.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/util/data/PlayerData.java
@@ -11,7 +11,11 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 
-
+/**
+ * Player data class to store vote counts, etc.
+ *
+ * @author m2en
+ */
 public class PlayerData {
 
     private FileConfiguration playerData = null;
