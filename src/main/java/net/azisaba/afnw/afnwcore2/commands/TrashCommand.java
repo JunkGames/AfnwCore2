5264a07bfe571891abe96c6443a07c31dfbccb40
m2en <merunno92@gmail.com>
2022-07-03 23:51:35 +0900

    chore: delete import

--- src/main/java/net/azisaba/afnw/afnwcore2/commands/TrashCommand.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/commands/TrashCommand.java
@@ -3,13 +3,11 @@ package net.azisaba.afnw.afnwcore2.commands;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
