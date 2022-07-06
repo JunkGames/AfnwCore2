f0f671b8b2ddc4c726336c3c5ee1a247483577da
m2en <merunno92@gmail.com>
2022-07-06 17:12:01 +0900

    feat: 自動セーブ間隔を変更できるように

--- src/main/java/net/azisaba/afnw/afnwcore2/util/data/PlayerDataSave.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/util/data/PlayerDataSave.java
@@ -9,15 +9,16 @@ import org.bukkit.scheduler.BukkitRunnable;
 public record PlayerDataSave(JavaPlugin plugin, PlayerData playerData) {
 
   public PlayerData playerData() {
+    int setPeriod = plugin.getConfig().getInt("settings.player-save-period", 120);
     new BukkitRunnable() {
       @Override
       public void run() {
-        Bukkit.getServer().broadcast(Component.text("プレイヤーデータセーブ中.......", NamedTextColor.YELLOW));
+        Bukkit.getServer().broadcast(Component.text("プレイヤーデータ: セーブ中....", NamedTextColor.YELLOW));
         playerData.savePlayerData();
         playerData.reloadPlayerData();
-        Bukkit.getServer().broadcast(Component.text("セーブ完了", NamedTextColor.YELLOW));
+        Bukkit.getServer().broadcast(Component.text("プレイヤーデータ: セーブ完了", NamedTextColor.YELLOW));
       }
-    }.runTaskTimer(plugin, 0, 20L * 60);
+    }.runTaskTimer(plugin, 0, 20L * setPeriod);
     return playerData;
   }
 }
