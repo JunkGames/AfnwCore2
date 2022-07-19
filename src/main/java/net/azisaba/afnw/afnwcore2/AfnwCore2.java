c8e573bee5b02d06db69dacb8edfa11fd50c503c
m2en <merunno92@gmail.com>
2022-07-19 20:08:22 +0900

    feat: 投票ボーナスを確認できるコマンドを追加

--- src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
@@ -3,6 +3,7 @@ package net.azisaba.afnw.afnwcore2;
 import java.util.Objects;
 import net.azisaba.afnw.afnwcore2.commands.AfnwCommand;
 import net.azisaba.afnw.afnwcore2.commands.BedCommand;
+import net.azisaba.afnw.afnwcore2.commands.BonusCommand;
 import net.azisaba.afnw.afnwcore2.commands.ConfigReloadCommand;
 import net.azisaba.afnw.afnwcore2.commands.EnderchestCommand;
 import net.azisaba.afnw.afnwcore2.commands.LobbyCommand;
@@ -85,6 +86,7 @@ public class AfnwCore2 extends JavaPlugin {
     Objects.requireNonNull(getCommand("pc")).setExecutor(new EnderchestCommand());
     Objects.requireNonNull(getCommand("trash")).setExecutor(new TrashCommand(this));
     Objects.requireNonNull(getCommand("maintenance")).setExecutor(new MaintenanceCommand());
+    Objects.requireNonNull(getCommand("bonus")).setExecutor(new BonusCommand(this, data));
     getLogger().info("コマンド 設定完了");
 
     if(getConfig().getBoolean("settings.maintenance-mode-toggle", false)) {
