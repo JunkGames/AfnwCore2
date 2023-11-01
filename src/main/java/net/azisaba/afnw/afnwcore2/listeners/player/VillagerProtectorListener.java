14d7618f27fe22478e4d675d10ddd9e3ac709f3d
acrylic-style <me@acrylicstyle.xyz>
2023-11-01 23:00:54 +0900

    update: more villager protection

--- src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/VillagerProtectorListener.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/VillagerProtectorListener.java
@@ -1,11 +1,14 @@
 package net.azisaba.afnw.afnwcore2.listeners.player;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class VillagerProtectorListener implements Listener {
@@ -19,6 +22,15 @@ public class VillagerProtectorListener implements Listener {
         }
         if (e.getDamager() instanceof Arrow arrow && arrow.getShooter() instanceof Player) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        if (e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+        if (!e.getBlock().getLocation().getNearbyEntitiesByType(Villager.class, 1, 2).isEmpty()) {
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(Component.text("このブロックは破壊できません！", NamedTextColor.RED));
         }
     }
 }
