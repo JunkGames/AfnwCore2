58f5edf80a116daa047a88de85e20f21ef22775f
acrylic-style <me@acrylicstyle.xyz>
2023-11-02 12:29:13 +0900

    update: play sound to everyone when using super afnw ticket

--- src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/SuperAfnwTicketListener.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/listeners/player/SuperAfnwTicketListener.java
@@ -23,10 +23,10 @@ public record SuperAfnwTicketListener(Plugin plugin) implements Listener {
         if (e.getPlayer().getGameMode() != GameMode.SURVIVAL) return;
         if (!"Super_Afnw_Ticket".equals(ItemUtil.getMythicType(e.getItem()))) return;
         e.getItem().setAmount(e.getItem().getAmount() - 1);
-        e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 1.5f);
-        Bukkit.getScheduler().runTaskLater(plugin, () -> e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 1.75f), 3);
-        Bukkit.getScheduler().runTaskLater(plugin, () -> e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 2.0f), 6);
         for (Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(e.getPlayer().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 1.5f);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 1.75f), 3);
+            Bukkit.getScheduler().runTaskLater(plugin, () -> player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1000f, 2.0f), 6);
             for (ItemStack value : player.getInventory().addItem(AfnwCommand.getRandomItem(1)).values()) {
                 ItemUtil.addToStashIfEnabledAsync(plugin, player.getUniqueId(), value);
             }
